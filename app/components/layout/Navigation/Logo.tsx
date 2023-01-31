f2cb706fd00e6e23d2e0a6c3cfdb771028106ff8
DerWaldgang <zxzsalam@mail.ru>
2023-02-01 03:13:57 +0600

    changed logo

--- app/components/layout/Navigation/Logo.tsx
+++ app/components/layout/Navigation/Logo.tsx
@@ -2,17 +2,18 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-import logoIMG from '@/assets/images/logo.svg'
+import logoIMG from '@/assets/images/software-cinema.png'
 
 const Logo = () => {
 	return (
-		<Link href={'/'} className="px-layout mb-10 block">
+		<Link href={'/'} className="px-5 mb-10 block">
 			<Image
 				src={logoIMG}
-				width={247}
-				height={34}
+				width={100}
+				height={50}
 				alt={'Next Cinema'}
 				draggable={false}
+				className='w-full h-11'
 			/>
 		</Link>
 	)
